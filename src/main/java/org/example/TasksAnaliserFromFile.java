9261adf8994ccdc0d7d5f9835d1ac1fd095e7538
lastM <cdrom2015@ya.ru>
2022-06-13 19:47:13 +0300

    first commit

---
+++ src/main/java/org/example/TasksAnaliserFromFile.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TasksAnaliserFromFile implements TasksAnalyzer {
+    public void analyze(String fileName){
+
+    }
+}
---
+++ src/main/java/org/example/TasksAnalyzer.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface TasksAnalyzer {
+    void analyze(String name);
+}
